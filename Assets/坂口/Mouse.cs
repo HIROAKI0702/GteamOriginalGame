0c8b968847085e39df9387e119678582d3e15e44
HIROAKI0702 <h1202315011@hiratagakuen.onmicrosoft.com>
2023-10-20 10:47:09 +0900

    アイテムをつかんだり離したりできるようにしました

--- Assets/坂口/Mouse.cs
+++ Assets/坂口/Mouse.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Mouse : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool isObjectSelected = false;//オブジェクトを選択しているかどうか
+    private Transform selectedObject;//選択されたオブジェクトのTransform
+    private Rigidbody2D rb2D; // 2DオブジェクトのRigidbody2Dコンポーネント
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.GetComponent<BoxCollider2D>();
+
+        //マウスの左ボタンがクリックされた場合
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            //クリックした位置にオブジェクトがあるかどうかをチェック
+            if(Physics.Raycast(ray, out hit))
+            {
+                //オブジェクトがクリックされた場合
+                if(hit.collider.gameObject.CompareTag("SelectableObject"))
+                {
+                    isObjectSelected = true;
+                    selectedObject = hit.collider.transform;
+                }
+            }
+        }
+
+        //マウスの左ボタンが離された場合
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(isObjectSelected)
+            {
+                isObjectSelected = false;
+                selectedObject = null;
+            }
+        }
+
+        //オブジェクトが選択されている場合、マウスの移動に応じてオブジェクトをドラッグ
+        if(isObjectSelected)
+        {
+            float distanceToCamera = Vector3.Distance(selectedObject.position, Camera.main.transform.position);
+            Vector3 targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceToCamera));
+            selectedObject.position = targetPosition;
+        }
     }
+
+
+
+    
+
 }
