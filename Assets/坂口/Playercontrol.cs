1d081fc63bb5c84540de789f975460d5b8c37909
HIROAKI0702 <h1202315011@hiratagakuen.onmicrosoft.com>
2023-10-25 10:09:24 +0900

    キャラクターが下に落ちたら初期位置に戻るプログラムを追加しました

--- Assets/坂口/Playercontrol.cs
+++ Assets/坂口/Playercontrol.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Playercontrol : MonoBehaviour
 {
@@ -18,12 +19,28 @@ public class Playercontrol : MonoBehaviour
 
         vx = speed;
         leftFlag = false;
-        
+
+        //このY座標より下へ落ちたらスタートへ戻る
+        float bottomY = Camera.main.transform.position.y
+                        - Camera.main.orthographicSize * 2;
+
+        //忍者のY座標がbottomYより低い
+        if (gameObject.transform.position.y < bottomY)
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+
     }
 
     void FixedUpdate()
     {
         this.transform.Translate(vx / 50, vy / 50, 0);
         //this.GetComponent<SpriteRenderer>().flipX = leftFlag;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
     }
 }
