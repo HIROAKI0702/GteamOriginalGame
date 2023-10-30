6fcb9d18ac57281110d27bc4f3b8bf2f453a88ca
HIROAKI0702 <h1202315011@hiratagakuen.onmicrosoft.com>
2023-10-30 10:53:53 +0900

    坂口のデータを復元しました

---
+++ Assets/坂口/Playercontrol.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Playercontrol : MonoBehaviour
+{
+    public float speed = 3;//初期スピードを3に設定;
+
+    float vx = 0;
+    float vy = 0;
+    bool leftFlag = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        vx = 0;
+        vy = 0; 
+
+        vx = speed;
+        leftFlag = false;
+
+        //このY座標より下へ落ちたらスタートへ戻る
+        float bottomY = Camera.main.transform.position.y
+                            - Camera.main.orthographicSize * 2;
+
+        //プレイヤーのY座標がbottomYより低い
+        if (gameObject.transform.position.y < bottomY)
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.Translate(vx / 50, vy / 50, 0);
+        //this.GetComponent<SpriteRenderer>().flipX = leftFlag;
+    }
+    
+
+}
